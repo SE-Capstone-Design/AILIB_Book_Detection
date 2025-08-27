bce988b448713426633fa4f8012299c4bcf18a42
KimSeoWon <ksw1912@gmail.com>
2025-08-28 00:52:49 +0900

    ocr 관련 수정

--- tracking_method/item.py
+++ tracking_method/item.py
@@ -9,7 +9,7 @@ class Item:
         self.ocr = ocr # 없을경우는 기본적 입력할때 None임 
         self.ocr_conf = ocr_conf  # None 또는 float
         self.row = int(row)
-        self.status = ItemStatus.NEW if (ocr_conf is not None and ocr_conf > 0.7) else ItemStatus.PENDING  #ocr 신뢰도가 0.7미만인 경우 보류로 설정 
+        self.status = ItemStatus.NEW if (ocr_conf is not None and ocr_conf > 0.2) else ItemStatus.PENDING  #ocr 신뢰도가 0.7미만인 경우 보류로 설정 
         self.parsed =parse_ddc_key(ocr) if ocr is not None else None # 없을경우는 None으로 
         self.updated_at = time.time()
 
