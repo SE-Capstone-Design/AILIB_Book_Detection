ae8fd85ccab9c5833bde117e88b5768d0e8a6ca5
KimSeoWon <ksw1912@gmail.com>
2025-10-21 15:12:58 +0900

    image_enhance_insert

--- tracking_method/processing.py
+++ tracking_method/processing.py
@@ -136,12 +136,16 @@ def enhance_img(img):
     img = cv2.resize(img, None, fx=5, fy=5, interpolation=cv2.INTER_CUBIC)
     # 3 그레이스케일 변환
       # ⚠️ BGR -> GRAY 변환
-    if len(img.shape) == 3 and img.shape[2] == 3:
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    elif len(img.shape) == 3 and img.shape[2] == 4:
-        gray = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
-    else:   
-        gray = img 
+    if len(img.shape) == 3:
+        if img.shape[2] == 3:
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        elif img.shape[2] == 4:
+            gray = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
+        else:
+            print(f"⚠️ Unexpected channel count: {img.shape[2]}")
+            gray = img[:, :, 0]
+    else:
+        gray = img  # 이미 그레이스케일이면 그대로 사용
     
     # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
