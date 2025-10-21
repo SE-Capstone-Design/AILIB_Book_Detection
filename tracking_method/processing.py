ec0dbac009a10e9bd9bb5b13f3ad15876bd5eef7
KimSeoWon <ksw1912@gmail.com>
2025-10-21 15:06:23 +0900

    image_enhance_insert

--- tracking_method/processing.py
+++ tracking_method/processing.py
@@ -133,8 +133,16 @@ def enhance_img(img):
     print("frame channels:", 1 if len(img.shape)==2 else img.shape[2])
     img = img.copy()
     #1. 2.해상도 키우기 (3~4배 정도)
-    gray = cv2.resize(img, None, fx=5, fy=5, interpolation=cv2.INTER_CUBIC)
+    img = cv2.resize(img, None, fx=5, fy=5, interpolation=cv2.INTER_CUBIC)
     # 3 그레이스케일 변환
+      # ⚠️ BGR -> GRAY 변환
+    if len(img.shape) == 3 and img.shape[2] == 3:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    elif len(img.shape) == 3 and img.shape[2] == 4:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
+    else:
+        gray = img 
+    
     # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
     # 4 대비 강화
